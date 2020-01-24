5e29fda8478d518505af559938aeed1323f34a58
nutbum05 <lukasz.orzech@gmail.com>
2020-01-24 12:24:36 +0000

    added ErrorBoundary component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import './styles/App.css';
-import Scroll from './Scroll'
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import '../containers/App.css';
+import Scroll from '../components/Scroll'
+import ErrorBoundary from '../components/ErrorBoundary'
 
 class App extends React.Component {
     constructor() {
@@ -22,22 +23,25 @@ class App extends React.Component {
         this.setState({ searchfield: e.target.value });
     };
     render() {
-        const filterRobots = this.state.robots.filter((robot) => {
+        const { robots } = this.state;
+        const filterRobots = robots.filter((robot) => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
-        if (this.state.robots.length === 0) {
-            return <hi>Loading</hi>;
-        } else {
-            return (
+        return !robots.length ?
+            <h1>Loading</h1>
+            :
+            (
                 <div className="tc">
                     <h1 className="f2">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filterRobots} />
+                        <ErrorBoundary>
+                            <CardList robots={filterRobots} />
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             );
-        }
     }
 }
+
 export default App;
